0a8b44ba921d67b9091c37c0796598dfa965db92
Codexophile <jaliya.chirantha@gmail.com>
2025-12-27 21:02:18 +0530

    fix: change overflow property from hidden to visible for better layout

--- content.js
+++ content.js
@@ -616,7 +616,7 @@
             position: relative;
             width: 100%;
             height: 100%;
-            overflow: hidden;
+          overflow: visible;
             border-radius: 4px;
         `;
 
